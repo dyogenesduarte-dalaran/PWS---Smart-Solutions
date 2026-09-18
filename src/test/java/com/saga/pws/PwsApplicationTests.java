cfbd985a87dad79c1515e299113e55ef18d77e47
dyogenesduarte-dalaran <dyogenes.duarte@gmail.com>
2026-09-17 23:50:01 -0300

    chore: initialize PWS Spring Boot project

---
+++ src/test/java/com/saga/pws/PwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saga.pws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
